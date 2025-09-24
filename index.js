795692230faf6d2a1c1d9af2690e9da9c3d07da6
Adhin Raj <adhinrajcr7@gmail.com>
2025-09-24 17:36:41 +0530

    question: upto 22

--- index.js
+++ index.js
@@ -1,43 +1,43 @@
 let classObj = {
-    "name" : "class A" ,
-    "teacherName" : "Mary" ,
-    "students" : [ 
-        {
-            "name" : "Ravi" ,
-            "id" : "101" ,
-            "marks" : [
-                {"subject" : "English" , "mark" : 25}, 
-                {"subject" : "Maths", "mark" : 48}, 
-                {"subject" : "Physics", "mark" : 40}, 
-                {"subject" : "Chemistry", "mark" : 30},
-                {"subject" : "Computer", "mark" : 20}
-            ]
-        },
-        {
-            "name" : "Aju" ,
-            "id" :  "102",
-            "marks" : [
-                {"subject" : "English" , "mark" : 35}, 
-                {"subject" : "Maths", "mark" : 38}, 
-                {"subject" : "Physics", "mark" : 33}, 
-                {"subject" : "Chemistry", "mark" : 34},
-                {"subject" : "Computer", "mark" : 30}
-            ]
-        },
-       
-        {
-            "name" : "Binu" ,
-            "id" : "104" ,
-            "marks" : [
-                {"subject" : "English" , "mark" : 49}, 
-                {"subject" : "Maths", "mark" : 49}, 
-                {"subject" : "Physics", "mark" : 47}, 
-                {"subject" : "Chemistry", "mark" : 46},
-                {"subject" : "Computer", "mark" : 50}
-            ]
-        }
-    ]
-}
+  name: "class A",
+  teacherName: "Mary",
+  students: [
+    {
+      name: "Ravi",
+      id: "101",
+      marks: [
+        { subject: "English", mark: 25 },
+        { subject: "Maths", mark: 48 },
+        { subject: "Physics", mark: 40 },
+        { subject: "Chemistry", mark: 30 },
+        { subject: "Computer", mark: 20 },
+      ],
+    },
+    {
+      name: "Aju",
+      id: "102",
+      marks: [
+        { subject: "English", mark: 35 },
+        { subject: "Maths", mark: 38 },
+        { subject: "Physics", mark: 33 },
+        { subject: "Chemistry", mark: 34 },
+        { subject: "Computer", mark: 30 },
+      ],
+    },
+
+    {
+      name: "Binu",
+      id: "104",
+      marks: [
+        { subject: "English", mark: 49 },
+        { subject: "Maths", mark: 49 },
+        { subject: "Physics", mark: 47 },
+        { subject: "Chemistry", mark: 46 },
+        { subject: "Computer", mark: 50 },
+      ],
+    },
+  ],
+};
 
 // 1.Write a function to print the name of the class: "class A".
 
@@ -46,7 +46,6 @@ let classObj = {
 // }
 
 // console.log(nameOfClass(classObj))
-
 
 // 2.Write a function to print the teacher's name: "Mary".
 
@@ -80,7 +79,6 @@ let classObj = {
 
 // console.log(subNameOfStudent(classObj,"Aju"))
 
-
 // 6.Write a function to print the marks of a specific student in all subjects.
 
 // function markOfStudent(classObj,studentName){
@@ -104,7 +102,6 @@ let classObj = {
 //     return classObj.students.filter(student=>student.name === studentName)[0].marks.reduce((acc,item)=>acc + item.mark,0)
 // }
 
-
 // console.log(totalMarks(classObj,"Binu"))
 
 // 9.Write a function to calculate and print the average marks for all students in a specific subject.
@@ -113,7 +110,6 @@ let classObj = {
 //     const numberOfStudent = classObj.students.length
 //     return classObj.students.map(student=> student.marks.filter(mark=>mark.subject === subName)).flat().reduce((acc,item)=>acc+item.mark,0)/numberOfStudent
 // }
-
 
 // console.log(avgMarkSub(classObj,"Physics"))
 
@@ -134,7 +130,6 @@ let classObj = {
 
 //     return classObj.students[index].name
 
-
 // }
 
 // console.log(studentWithHighestMark(classObj,"Computer"))
@@ -146,8 +141,195 @@ let classObj = {
 //     const smallest = Math.min(...studentMarks)
 //     const index = studentMarks.indexOf(smallest)
 
+//     return classObj.students[index].name
+// }
+
+// console.log(studentWithLowestMarks(classObj,"Physics"))
+
+// 13.Write a functoin to find and print the student with the highest total marks
+
+// function topperStudent(classObj){
+//     const studentMarks = classObj.students.map(student=>student.marks.reduce((total,subMark)=>total + subMark.mark,0))
+//     const highestMark = Math.max(...studentMarks)
+//     const index = studentMarks.indexOf(highestMark)
 
 //     return classObj.students[index].name
 // }
 
-// console.log(studentWithLowestMarks(classObj,"English"))
+// console.log(topperStudent(classObj))
+
+// 14.Write a function to find and print the student with the lowest total marks.
+
+// function lowerStudent(classObj){
+//     const studentMarks = classObj.students.map(student=> student.marks.reduce((total,subMark)=>total + subMark.mark,0))
+//     const lowestMark  = Math.min(...studentMarks)
+//     const index = studentMarks.indexOf(lowestMark)
+
+//     return classObj.students[index].name
+// }
+
+// console.log(lowerStudent(classObj))
+
+// 15.Write a function to find and print the subject with the highest average marks.
+
+// function subWithHighestAvgMark(classObj){
+//     let subjectsMark = {}
+//     const avgMarkSub = classObj.students.map(student=> student.marks).flat()
+
+//      for(const item of avgMarkSub){
+//         if(!subjectsMark[item.subject]){
+//             subjectsMark[item.subject] = item.mark
+//         }
+//         else{
+//             subjectsMark[item.subject] = subjectsMark[item.subject] + item.mark
+//         }
+//     }
+
+//    const markArr = Object.values(subjectsMark)
+//    const numberOfStudent = classObj.students.length
+//    const avgMarkArr = markArr.map(item=> item/numberOfStudent)
+//    const highestAvg = avgMarkArr.indexOf(Math.max(...avgMarkArr))
+//    return Object.entries(subjectsMark)[highestAvg][0]
+// }
+
+// console.log(subWithHighestAvgMark(classObj))
+
+// 16.Write a function to find and print the subject with the lowest average marks.
+
+// function subWithLowestAvgMark(){
+//     let subMarks = {}
+
+//     const markArr  = classObj.students.map(student=> student.marks).flat()
+//     for(const item of markArr){
+//         if(!subMarks[item.subject]){
+//             subMarks[item.subject] = item.mark
+//         }
+//         else{
+//             subMarks[item.subject] = subMarks[item.subject] + item.mark
+//         }
+//     }
+
+//     const eachSubMark = Object.values(subMarks)
+//     const totalStudent = classObj.students.length
+//     const avgMarkArr = eachSubMark.map((item)=> item/totalStudent)
+//     const indexOfLowest = avgMarkArr.indexOf(Math.min(...avgMarkArr) )
+//     return Object.entries(subMarks)[indexOfLowest][0]
+
+// }
+
+// console.log(subWithLowestAvgMark(classObj))
+
+// 17.Write a function to calculate and print the overall average marks for the class.
+
+// function overallAvgMark(classObj){
+//     const classAvg = classObj.students.map(student=>student.marks.reduce((total,item)=> total + item.mark,0)).reduce((total,mark)=>total + mark,0)
+//     const totalStudent = classObj.students.length
+
+//     return classAvg/totalStudent
+// }
+
+// console.log(overallAvgMark(classObj))
+
+// 18.Write a funcition to calculate and print the overall total mark for the class
+
+// function overallMark(classObj){
+//     return classObj.students.map(student=>student.marks.reduce((total,submark)=> total + submark.mark,0)).reduce((total,item)=> total+item,0)
+// }
+
+// console.log(overallMark(classObj))
+
+// 19.Write a function to calculate and print the average marks for each subject
+
+// function averageMark(classObj){
+//     let totalMarkEachSub = {}
+//     let avgMarkEachSub = {}
+//    const noOfStudents  = classObj.students.length
+//    const marks =  classObj.students.map(student=> student.marks).flat()
+
+//     for(const item of marks){
+//         if(!totalMarkEachSub[item.subject]){
+//             totalMarkEachSub[item.subject] = item.mark
+//         }
+//         else{
+//             totalMarkEachSub[item.subject] = totalMarkEachSub[item.subject] + item.mark
+//         }
+//     }
+
+//     for(const key in totalMarkEachSub){
+//             avgMarkEachSub[key] = totalMarkEachSub[key]/noOfStudents
+//     }
+
+//     return avgMarkEachSub
+// }
+
+// console.log(averageMark(classObj))
+
+// 20.Write a function to calculate and print the total marks for each subject.
+
+// function totalMarkEachSub(classObj){
+//     let totalMark = {}
+
+//     const marks = classObj.students.map(student=>student.marks).flat()
+
+//     for(const subMark of marks){
+//         if(!totalMark[subMark.subject]){
+//             totalMark[subMark.subject] = subMark.mark
+//         }
+//         else{
+//             totalMark[subMark.subject] = totalMark[subMark.subject] + subMark.mark
+//         }
+//     }
+//     return totalMark
+// }
+
+// console.log(totalMarkEachSub(classObj))
+
+// 21.Write a function to find and print the subject with the highest total marks.
+
+// function subWithhighestMark(classObj) {
+//   let totalMark = {};
+
+//   const marks = classObj.students.map((student) => student.marks).flat();
+
+//   for (const subMark of marks) {
+//     if (!totalMark[subMark.subject]) {
+//       totalMark[subMark.subject] = subMark.mark;
+//     } else {
+//       totalMark[subMark.subject] = totalMark[subMark.subject] + subMark.mark;
+//     }
+//   }
+//   const largest = Math.max(...Object.values(totalMark));
+
+//   const index = Object.values(totalMark).indexOf(largest);
+
+//   return Object.keys(totalMark)[index];
+// }
+
+// console.log(subWithhighestMark(classObj));
+
+// 22.Write a function to find and print the subject with the lowest total marks.
+
+// function subWithLowestTotalMark(classObj){
+//     let totalMark = {}
+
+//     const marks = classObj.students.map((student)=> student.marks).flat()
+
+//     for(let subMark of marks){
+//         if(!totalMark[subMark.subject]){
+//             totalMark[subMark.subject] = subMark.mark
+//         }
+//         else{
+//             totalMark[subMark.subject] = totalMark[subMark.subject] + subMark.mark
+//         }
+//     }
+
+//     const smallest = Math.min(...(Object.values(totalMark)))
+//     const index = Object.values(totalMark).indexOf(smallest)
+
+//     return Object.keys(totalMark)[index]
+// }
+
+// console.log(subWithLowestTotalMark(classObj))
+
+// 23.Write a function to find and print the student(s) with the highest average marks.
+
